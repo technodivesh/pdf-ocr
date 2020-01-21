5824ec3f18af0ad69a42cac76bd1def8ef187dfd
technodivesh <technodivesh@gmail.com>
2020-01-21 17:36:34 +0530

    21jan

--- run.py
+++ run.py
@@ -21,13 +21,41 @@ def show_wait_destroy(winname, img):
     cv.waitKey(0)
     cv.destroyWindow(winname)
 
+
 def get_contour_precedence(contour, cols):
-    print("contour, cols--", contour, cols)
     origin = cv.boundingRect(contour)
-    print("origin--",origin, "-----",origin[1] * cols + origin[0])
-    # return origin[1] * cols + origin[0]
-    # return origin[1] * cols + origin[0]
-    return cols + origin[0]
+    return origin[1] * cols # + origin[0]
+
+def order_by_top_bottom(contour, rows):
+    origin = cv.boundingRect(contour)
+    return origin[0] * rows # + origin[0]
+
+def get_contour_precedence2(contour, cols):
+
+    M = cv.moments(contour)
+    print(M)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+    print(cx,cy)
+    print("--------------------#----",len(contour), cy * cols )#+ cx)
+    return cy * cols # + cx
+
+def row_range_tuple(contours):
+
+    range_list = []
+    for i, cnt in enumerate(contours):
+        x,y,w,h = cv.boundingRect(cnt)
+        if i == 0:
+            range_list.append((y,y+h))
+        elif y < range_list[-1][0]:
+            range_list.append((y,y+h))
+        else:break
+    
+    # print(range_list)
+    return range_list
+
+
+
 
 
 if __name__ == "__main__":
@@ -76,22 +104,26 @@ if __name__ == "__main__":
 
                 # contours = sorted(contours, key=lambda ctr: cv.boundingRect(ctr)[0] + cv.boundingRect(ctr))
                 # contours.sort(key=lambda x:get_contour_precedence(x, img.shape[1]))
-                contours.sort(key=lambda x:get_contour_precedence(x, img.shape[1]))
+                # contours.sort(key=lambda x:get_contour_precedence2(x, img.shape[1]))
+
+                contours = list(filter(lambda x: len(x) > 2, contours))
+                contours.sort(key=lambda x:order_by_top_bottom(x, img.shape[0]))
+                row_range_tuple = row_range_tuple(contours)
+
+                print(len(contours))
+                # contours = list(map(lambda x: len(x) > 2, contours))
+                # contours.sort(key=lambda x:get_contour_precedence(x, img.shape[1]))
+                # contours.sort(key=lambda x:get_contour_precedence2(x, img.shape[1]))
+
                 cv.drawContours(img, contours, -1, (0,255,0), -1)
-
-
-
                 for i,cnt in enumerate(contours):
-                    # print(cnt)
+                    print("---------------------------------",len(cnt),i)
+                    print(cnt)
                     x,y,w,h = cv.boundingRect(cnt)
-                    # compute the center of the contour
-                    # M = cv.moments(cnt)
-                    # cX = int(M["m10"] / M["m00"])
-                    # cY = int(M["m01"] / M["m00"])
-
-                    x = int(x + w / 2)
-                    y = int(y + h / 2)
-                    cv.putText(img, str(i), (x,y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                    print(x,y,w,h)
+                    xc = int(x + w / 2)
+                    yc = int(y + h / 2)
+                    cv.putText(img, str(i), (xc,yc), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
                 
                 cv.imwrite(f'{outbox}/pg-{pg_num}-img.png',img)
                 show_wait_destroy('img',img)
