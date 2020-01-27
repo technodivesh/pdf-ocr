9ee0c78b16303b510837df1d28fa7d2523f5897b
technodivesh <technodivesh@gmail.com>
2020-01-27 15:06:10 +0530

    27jan

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import glob
 from pdf2image import convert_from_path
 from PIL import Image
 import numpy as np
-# import cv2 
 import cv2 as cv
 from matplotlib import pyplot as plt 
 import os
@@ -70,23 +69,21 @@ def get_row_list(contours):
     return tuple(row_list)
 
 
-
-
-
 if __name__ == "__main__":
 
     INPUT_DIR = settings.INBOX
 
     pdfs = glob.glob(f'{INPUT_DIR}/*229.pdf')
-    # pdfs = glob.glob(f'{INPUT_DIR}/22123708.pdf')
+
     for pdf in pdfs[:1]:
         print("pdf--",pdf)
         pdf_obj = ReadPdf(pdf)
 
         ############# To read page one  by one ##############
-        FRAMES = [] # one dataframe per page
+        COLUMNS = pd.DataFrame()
+        result_df = pd.DataFrame()
 
-        for pg_num in range(pdf_obj.num_of_pages())[2:4]:
+        for pg_num in range(pdf_obj.num_of_pages())[2:3]:
 
             img,status = pdf_obj.read_page(pg_num)
             # if status:
@@ -97,13 +94,12 @@ if __name__ == "__main__":
 
             
             gray,table,inverted_table,head,no_grid = pdfPageObj.get_grid()
-            # pdfPageObj.get_grid()
 
             # show_wait_destroy('gray',gray)
             # show_wait_destroy('table',table)
             # show_wait_destroy('head',head)
             # show_wait_destroy('no_grid',no_grid)
-            show_wait_destroy('inverted_table',inverted_table)
+            # show_wait_destroy('inverted_table',inverted_table)
 
             # To write in file
             pdfNameOnly = os.path.splitext(os.path.basename(pdf))[0]
@@ -119,22 +115,20 @@ if __name__ == "__main__":
             # cv.imwrite(f'{outbox}/pg-{pg_num}-inverted_table.png',inverted_table)
 
             contours, hierarchy = cv.findContours(inverted_table, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
             contours = list(filter(lambda x: len(x) > 2, contours))
             contours.sort(key=lambda x:get_contour_precedence(x, img.shape[1]))
 
-
-            print(len(contours))
-            cv.drawContours(img, contours, -1, (0,255,0), -1)
-            for i,cnt in enumerate(contours):
-                x,y,w,h = cv.boundingRect(cnt)
-                # print(x,y,w,h)
-                xc = int(x + w / 2)
-                yc = int(y + h / 2)
-                cv.putText(img, str(i), (xc,yc), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            # print(len(contours))
+            # cv.drawContours(img, contours, -1, (0,255,0), -1)
+            # for i,cnt in enumerate(contours):
+            #     x,y,w,h = cv.boundingRect(cnt)
+            #     # print(x,y,w,h)
+            #     xc = int(x + w / 2)
+            #     yc = int(y + h / 2)
+            #     cv.putText(img, str(i), (xc,yc), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
             
-            cv.imwrite(f'{outbox}/pg-{pg_num}-detection.png',img)
-            show_wait_destroy('img',img)
+            # cv.imwrite(f'{outbox}/pg-{pg_num}-detection.png',img)
+            # show_wait_destroy('img',img)
 
             # table cell bb
             table_cells_bbs = get_row_list(contours) # tuple of bounding boxes
@@ -146,19 +140,46 @@ if __name__ == "__main__":
             table_cells_imgs = filter(lambda x: len(x) == no_of_data_cols,table_cells_imgs)
 
 
-            # data = (('A','B','C','D'),(1,2,3,4),(5,6,7,8),(9,10,11,12))
-            df = pd.DataFrame(tuple(table_cells_imgs)[:5])
+            #------------------ for testing only ------------------#
+            for cell_img_list in list(table_cells_imgs):
+                print(len(cell_img_list))
+                for cell_img in cell_img_list:
+                    print("------")
+                    cell_img[cell_img > 200] = 255
+                    ###############################
+                    ###############################
+                    ###############################
+                    print(image_to_string(cell_img))
+                    ###############################
+                    ###############################
+                    ###############################
+                    show_wait_destroy(f'cell_img',cell_img)
+            cv.destroyAllWindows()
+            exit()
+            #------------------- for testing only ------------------#
 
+            # Create DataFrame
+            df = pd.DataFrame(tuple(table_cells_imgs))
+
+            # Apply function to all cells
+            ###############################
+            ###############################
+            ###############################
             df = df.applymap(image_to_string)
-            df.columns = df.iloc[0]
+            ###############################
+            ###############################
+            ###############################
+
+            # df.columns = df.iloc[0]
+
+            if COLUMNS.empty:
+                COLUMNS = df.iloc[0]
             df.drop(df.index[0],inplace=True)
 
-            FRAMES.append(df)
+            result_df = result_df.append(df,ignore_index=True)
 
-        print(FRAMES)
-        result_df = pd.concat(FRAMES,axis=0,ignore_index=True)
-
-        print(result_df)
+        result_df.columns = COLUMNS
+        
 
         # Create a Pandas Excel writer using XlsxWriter as the engine.
         writer = pd.ExcelWriter(f'{outbox}.xlsx', engine='xlsxwriter')
@@ -169,63 +190,5 @@ if __name__ == "__main__":
         # Close the Pandas Excel writer and output the Excel file.
         writer.save()
 
-                # print(df.columns)
-
-                # for table_cells_img in table_cells_imgs:
-                #     print(len(table_cells_img))
-                    # IMG = np.hstack(([img for img in table_cells_img]))
-                    # show_wait_destroy(f'cell_img',cell_img)
-
-
-                # for cell_img_row in table_cells_imgs:
-
-                #     print("=================--*texts--==============")
-                #     map()
-                #     texts = map(image_to_string,cell_img_row)
-                #     print("--*texts--", *texts)
-
-
-                # #------------------ for demo ------------------#
-                # for cell_img_list in list(table_cells_imgs)[-3:-2]:
-                #     print(len(cell_img_list))
-                #     for cell_img in cell_img_list:
-                #         print("------")
-                #         cell_img[cell_img > 200] = 255
-                #         print(image_to_string(cell_img))
-                #         show_wait_destroy(f'cell_img',cell_img)
-                # #------------------- for demo ------------------#
-
-
-
-                # print(len(row_list))
-
-                # ------------------------------------------------------- #
-                # Under testing
-                # contours.sort(key=lambda x:ordering(x, img.shape[0]))
-                # row_range_list = row_range_list(contours)
-                # table = dict.fromkeys(row_range_list,[])
-
-                # for tup_range,value_list in table.items():
-                #     table[tup_range] = list(filter(lambda cnt: tup_range[0] <  math.ceil(( cnt[0][0][1]+ cnt[1][0][1])/2)  < tup_range[1], contours ))
-                # -------------------------------------------------------- #
-
-
-
-                # print(len(contours))
-                # cv.drawContours(img, contours, -1, (0,255,0), -1)
-                # for i,cnt in enumerate(contours):
-                #     x,y,w,h = cv.boundingRect(cnt)
-                #     # print(x,y,w,h)
-                #     xc = int(x + w / 2)
-                #     yc = int(y + h / 2)
-                #     cv.putText(img, str(i), (xc,yc), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-                
-                # cv.imwrite(f'{outbox}/pg-{pg_num}-detection.png',img)
-                # show_wait_destroy('img',img)
-
-
-
-            # cv.destroyAllWindows()
-        ############# To read page one  by one ##############
-
+    
 
--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ OUTBOX = "/home/root1/AllErrorFiles/testoutbox"
 # INBOX = "/home/root1/AllErrorFiles/phase2"
 # OUTBOX = "/home/root1/AllErrorFiles/phase2/outbox"
 DPI = 200
-DEBUG = False
+DEBUG = True
 
 TRAINING = True
